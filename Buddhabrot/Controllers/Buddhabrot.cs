841aae0dff021152b17c79a5834e9350be35b26b
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2022-02-20 15:57:21 -0500

    Hello, world!

---
+++ Buddhabrot/Controllers/Buddhabrot.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+namespace Buddhabrot.Controllers
+{
+	/// <summary>
+	/// Buddhabrot API controller.
+	/// </summary>
+	[Route("api/[controller]")]
+	[ApiController]
+	public class Buddhabrot : ControllerBase
+	{
+		/// <summary>
+		/// Gets a buddhabrot image.
+		/// </summary>
+		/// <returns>A buddhabrot image.</returns>
+		[HttpGet]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status500InternalServerError)]
+		public ActionResult Get()
+		{
+			try
+			{
+				return Ok("Hello, world!");
+			}
+			catch (Exception ex)
+			{
+				Log.Error(ex, "Rendering failed.");
+				return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+			}
+		}
+	}
+}
---
+++ Buddhabrot/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.OpenApi.Models;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+	.WriteTo.Console()
+	.CreateBootstrapLogger();
+
+Log.Information("Starting up...");
+
+try
+{
+	var builder = WebApplication.CreateBuilder(args);
+
+	// Add services to the container.
+
+	builder.Services.AddControllers();
+	// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+	builder.Services.AddEndpointsApiExplorer();
+	builder.Services.AddSwaggerGen(c =>
+	{
+		c.SwaggerDoc("v1", new OpenApiInfo
+		{
+			Version = "v1",
+			Title = "Buddhabrot API",
+			Description = "A demo project to render Buddhabrot images.",
+			Contact = new OpenApiContact
+			{
+				Name = "Evan Beeton",
+				Url = new Uri("https://github.com/ebeeton")
+			}
+		});
+	});
+	builder.Host.UseSerilog((context, config) => config.WriteTo.Console()
+													   .ReadFrom.Configuration(context.Configuration));
+
+	var app = builder.Build();
+
+	// Configure the HTTP request pipeline.
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseSwagger();
+		app.UseSwaggerUI();
+	}
+
+	app.UseHttpsRedirection();
+
+	app.UseAuthorization();
+
+	app.MapControllers();
+
+	app.Run();
+}
+catch (Exception ex)
+{
+	Log.Fatal(ex, "Unhandled exception.");
+}
+finally
+{
+	Log.Information("Shutting down gracefully.");
+	Log.CloseAndFlush();
+}
