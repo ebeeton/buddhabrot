530d2a7716af41559c6a21af2d5d3b33e19bf1f1
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2022-02-21 17:57:22 -0500

    Simple grayscale Mandelbrot rendering.

---
+++ Buddhabrot.Core/LinearScale.cs
@@ -0,0 +1,23 @@
+﻿namespace Buddhabrot.Core
+{
+	/// <summary>
+	/// Performs linear scales.
+	/// </summary>
+	public static class LinearScale
+	{
+		/// <summary>
+		/// Linearly scales a value in one range to another.
+		/// </summary>
+		/// <param name="val">Value to scale.</param>
+		/// <param name="minScaleFrom">Minimum value of the range scaling from.</param>
+		/// <param name="maxScaleFrom">Maximum value of the range scaling from.</param>
+		/// <param name="minScaleTo">Minimum value of the range scaling to.</param>
+		/// <param name="maxScaleTo">Maximum value of the range scaling to.</param>
+		/// <returns>Linearly scaled value.</returns>
+		public static float Scale(float val, float minScaleFrom, float maxScaleFrom, float minScaleTo, float maxScaleTo)
+		{
+			// https://stackoverflow.com/a/5295202
+			return (maxScaleTo - minScaleTo) * (val - minScaleFrom) / maxScaleFrom - minScaleFrom + minScaleTo;
+		}
+	}
+}
--- Buddhabrot.Core/Renderer.cs
+++ Buddhabrot.Core/Renderer.cs
@@ -1,5 +1,6 @@
 ﻿using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
+using System.Numerics;
 
 namespace Buddhabrot.Core
 {
@@ -8,7 +9,21 @@ namespace Buddhabrot.Core
 	/// </summary>
 	public class Renderer
 	{
+		/// <summary>
+		/// # https://en.wikipedia.org/wiki/Plotting_algorithms_for_the_Mandelbrot_set
+		/// "Because no complex number with a real or imaginary part greater
+		/// than 2 can be part of the set, a common bailout is to escape when
+		/// either coefficient exceeds 2."
+		/// </summary>
+		private const double Bailout = 2.0;
+
 		private const int RGBBytesPerPixel = 3;
+		private const int MaxIterations = 128;
+
+		private const float MinX = -2;
+		private const float MaxX = 0.47f;
+		private const float MinY = -1.12f;
+		private const float MaxY = 1.12f;
 
 		/// <summary>
 		/// Test render method.
@@ -16,16 +31,32 @@ namespace Buddhabrot.Core
 		/// <returns>A task representing the work to render the image.</returns>
 		public static async Task<MemoryStream> RenderPng(int width, int height)
 		{
-			int rowLength = width * RGBBytesPerPixel;
+			var bytesPerLine = width * RGBBytesPerPixel;
 
-			var data = new byte[height * rowLength];
+			var data = new byte[height * bytesPerLine];
 
 			for (int y = 0; y < height; ++y)
-				for (int x = 0; x < rowLength; x += 3)
+			{
+				var imaginary = LinearScale.Scale(y, 0, height, MinY, MaxY);
+
+				for (int x = 0; x < bytesPerLine; x += 3)
 				{
-					var index = y * rowLength + x;
-					data[index] = (byte)((float)x / rowLength * 255);
+					var real = LinearScale.Scale(x, 0, bytesPerLine, MinX, MaxX);
+
+					var iterations = GetPixel(new Complex(real, imaginary));
+					if (iterations == 0)
+					{
+						// Leave points in the set black.
+						continue;
+					}
+
+					// Simple grayscale for now.
+					var color = (byte)((double)iterations / MaxIterations * 255);
+					data[y * bytesPerLine + x] =
+					data[y * bytesPerLine + x + 1] =
+					data[y * bytesPerLine + x + 2] = color;
 				}
+			}
 
 			using var image = Image.LoadPixelData<Rgb24>(data, width, height);
 			var output = new MemoryStream();
@@ -33,6 +64,24 @@ namespace Buddhabrot.Core
 			output.Seek(0, SeekOrigin.Begin);
 
 			return output;
+		}
+
+		private static int GetPixel(Complex c)
+		{
+			var z = new Complex(0, 0);
+			for (int i = 0; i < MaxIterations; ++i)
+			{
+				if (z.Magnitude > Bailout)
+				{
+					// Not in the set.
+					return i;
+				}
+
+				z = z * z + c;
+			}
+
+			// "Probably" in the set.
+			return 0;
 		}
 	}
 }
