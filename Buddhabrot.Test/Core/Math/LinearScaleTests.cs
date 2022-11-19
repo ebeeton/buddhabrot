2096bffd7561015af2a05071e5605267c66061f4
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2022-11-19 11:41:55 -0500

    Reorganize tests to match project namespaces.

---
+++ Buddhabrot.Test/Core/Math/LinearScaleTests.cs
@@ -0,0 +1,26 @@
+using Buddhabrot.Core.Math;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Buddhabrot.Test.Core.Math
+{
+    [TestClass]
+    public class LinearScaleTests
+    {
+        /// <summary>
+        /// "Close enough".
+        /// </summary>
+        const double Delta = 1e-6;
+
+        [DataRow(5, 0, 10, 0, 100, 50)]
+        [DataRow(50, 0, 100, 0, 1, 0.5)]
+        [DataRow(75, 0, 100, 0, 1, 0.75)]
+        [DataRow(0, -2, 0.47, 0, 3072, 2487.449392712551)]
+        [DataTestMethod]
+        public void LinearScale_WithRanges_ScalesCorrectly(double val, double minScaleFrom, double maxScaleFrom, double minScaleTo, double maxScaleTo, double expected)
+        {
+            var actual = Linear.Scale(val, minScaleFrom, maxScaleFrom, minScaleTo, maxScaleTo);
+
+            Assert.AreEqual(expected, actual, Delta);
+        }
+    }
+}
