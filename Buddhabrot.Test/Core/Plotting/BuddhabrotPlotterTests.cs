150decb6404094e9661367de4159e9ff340165cc
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2022-11-19 11:59:26 -0500

    Add test for BuddhabrotPlotter.

---
+++ Buddhabrot.Test/Core/Plotting/BuddhabrotPlotterTests.cs
@@ -0,0 +1,32 @@
+﻿using Buddhabrot.Core.Parameters;
+using Buddhabrot.Core.Plotting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SixLabors.ImageSharp;
+using System.Threading.Tasks;
+
+namespace Buddhabrot.Test.Core.Plotting
+{
+	[TestClass]
+	public class BuddhabrotPlotterTests
+	{
+		[TestMethod]
+		public async Task PlotPng_WithParameters_ReturnsImageWithCorrectImageSize()
+		{
+			const int Height = 64, Width = 64, MaxIterations = 32;
+			var parameters = new BuddhabrotParameters
+			{
+				Height = Height,
+				Width = Width,
+				MaxIterations = MaxIterations,
+				SampleSize = 0.1,
+				MaxSampleIterations = MaxIterations,
+			};
+			var plotter = new BuddhabrotPlotter(parameters);
+
+			var image = Image.Load(await plotter.PlotPng());
+
+			Assert.AreEqual(Height, image.Height);
+			Assert.AreEqual(Width, image.Width);
+		}
+	}
+}
