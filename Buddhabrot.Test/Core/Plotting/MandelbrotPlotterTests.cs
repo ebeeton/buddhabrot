d7f6220cc643201f8ceb1ad41aee29f09cc5372c
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2022-11-19 11:55:52 -0500

    Add test for MandelbrotPlotter.

---
+++ Buddhabrot.Test/Core/Plotting/MandelbrotPlotterTests.cs
@@ -0,0 +1,30 @@
+﻿using Buddhabrot.Core.Parameters;
+using Buddhabrot.Core.Plotting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SixLabors.ImageSharp;
+using System.Threading.Tasks;
+
+namespace Buddhabrot.Test.Core.Plotting
+{
+	[TestClass]
+	public class MandelbrotPlotterTests
+	{
+		[TestMethod]
+		public async Task PlotPng_WithParameters_ReturnsImageWithCorrectImageSize()
+		{
+			const int Height = 64, Width = 64, MaxIterations = 32;
+			var parameters = new MandelbrotParameters
+			{
+				Height = Height,
+				Width = Width,
+				MaxIterations = MaxIterations
+			};
+			var plotter = new MandelbrotPlotter(parameters);
+
+			var image = Image.Load(await plotter.PlotPng());
+
+			Assert.AreEqual(Height, image.Height);
+			Assert.AreEqual(Width, image.Width);
+		}
+	}
+}
