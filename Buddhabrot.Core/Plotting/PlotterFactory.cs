1f37d2a21dcdc8308b3a8403001576e915150b8b
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2022-12-03 15:31:49 -0500

    Add PlotterFactory and tests.

---
+++ Buddhabrot.Core/Plotting/PlotterFactory.cs
@@ -0,0 +1,49 @@
+﻿using Buddhabrot.Core.Models;
+using Newtonsoft.Json;
+
+namespace Buddhabrot.Core.Plotting
+{
+	/// <summary>
+	/// Creates instances of classes derived from <see cref="Plotter"/>.
+	/// </summary>
+	public static class PlotterFactory
+	{
+		/// <summary>
+		/// Get an instance of a class derived from <see cref="Plotter"/> by <see cref="PlotType"/>.
+		/// </summary>
+		/// <param name="plotType"><see cref="PlotType"/>.</param>
+		/// <param name="paramsJson">Plotter-specific parameters in JSON.</param>
+		/// <returns>Instance of a class derived from <see cref="Plotter"/>.</returns>
+		public static Plotter GetPlotter(PlotType plotType, string paramsJson)
+		{
+			if (string.IsNullOrWhiteSpace(paramsJson))
+			{
+				throw new ArgumentNullException(nameof(paramsJson));
+			}
+
+			switch (plotType)
+			{
+				case PlotType.Mandelbrot:
+					{
+						var parameters = JsonConvert.DeserializeObject<MandelbrotParameters>(paramsJson);
+						if (parameters == null)
+						{
+							throw new ArgumentException($"Could not deserialize type {nameof(MandelbrotParameters)}.", nameof(paramsJson));
+						}
+						return new MandelbrotPlotter(parameters);
+					}
+				case PlotType.Buddhabrot:
+					{
+						var parameters = JsonConvert.DeserializeObject<BuddhabrotParameters>(paramsJson);
+						if (parameters == null)
+						{
+							throw new ArgumentException($"Could not deserialize type {nameof(BuddhabrotParameters)}.", nameof(paramsJson));
+						}
+						return new BuddhabrotPlotter(parameters);
+					}
+				default:
+					throw new ArgumentException("Unsupported plot type.");
+			}
+		}
+	}
+}
---
+++ Buddhabrot.Test/Core/Plotting/PlotterFactoryTests.cs
@@ -0,0 +1,32 @@
+﻿using Buddhabrot.Core.Models;
+using Buddhabrot.Core.Plotting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Buddhabrot.Test.Core.Plotting
+{
+	[TestClass]
+	public class PlotterFactoryTests
+	{
+		[TestMethod]
+		public void GetPlotter_WithMandelbrotType_ReturnsMandelbrotPlotter()
+		{
+			var type = PlotType.Mandelbrot;
+			var parameters = "{\"Width\":1024,\"Height\":768,\"MaxIterations\":128}";
+
+			var plotter = PlotterFactory.GetPlotter(type, parameters);
+
+			Assert.IsNotNull(plotter as MandelbrotPlotter);
+		}
+
+		[TestMethod]
+		public void GetPlotter_WithBuddhabrotType_ReturnsMandelbrotPlotter()
+		{
+			var type = PlotType.Buddhabrot;
+			var parameters = "{\"Width\":1024,\"Height\":768,\"MaxIterations\":16384,\"MaxSampleIterations\":2048,\"SampleSize\":0.01,\"Passes\":32,\"Grayscale\":false}";
+
+			var plotter = PlotterFactory.GetPlotter(type, parameters);
+
+			Assert.IsNotNull(plotter as BuddhabrotPlotter);
+		}
+	}
+}
